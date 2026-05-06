9c5b8168e7be43ac3e4004c6f68429b7e89b6b67
Bautista <bautistacorte05@gmail.com>
2026-05-06 10:12:49 -0300

    feat: add Mongoose models for Product and Cart

---
+++ src/models/Product.js
@@ -0,0 +1,17 @@
+import { Schema, model } from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+const productSchema = new Schema({
+  title:       { type: String, required: true },
+  description: { type: String, required: true },
+  code:        { type: String, required: true, unique: true },
+  price:       { type: Number, required: true },
+  status:      { type: Boolean, default: true },
+  stock:       { type: Number, required: true },
+  category:    { type: String, required: true },
+  thumbnails:  { type: [String], default: [] },
+});
+
+productSchema.plugin(mongoosePaginate);
+
+export const Product = model('Product', productSchema);
